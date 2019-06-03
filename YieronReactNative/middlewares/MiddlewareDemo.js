1a203d2885d68151d78ce40c5fc63f4e018ac4a0
Yieron东 <2293264218@qq.com>
2019-06-03 19:02:47 +0800

    【端】RND 【Task】学习Redux 【修改内容】 【后续跟进】

---
+++ YieronReactNative/middlewares/MiddlewareDemo.js
@@ -0,0 +1,11 @@
+function applyMiddleware(store, middlewares) {
+    middlewares = middlewares.slice()
+    middlewares.reverse()
+
+    let dispatch = store.dispatch
+    middlewares.forEach(middleware =>
+        dispatch = middleware(store)(dispatch)
+    )
+
+    return Object.assign({}, store, { dispatch })
+}
